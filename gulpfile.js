a3aedd54b5b7f5c5537afbf79f4816d20643849b
Matteo Rosati <mrosati@h-art.it>
2015-05-14 09:47:47 +0200

    init project, first gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp'),
+    minifyHTML = require('gulp-minify-html'),
+    browserSync = require('browser-sync').create(),
+    reload = browserSync.reload,
+    sass = require('gulp-sass');
+
+var paths = {
+    'app': {
+        'sass': 'app/sass/**/*.scss',
+        'html': 'app/**/*.html'
+    },
+    'dist': {
+        'sass': 'dist/css',
+        'html': 'dist'
+    }
+}
+
+gulp.task('html', function() {
+    var options = {
+        conditionals: true,
+        spare: true
+    };
+
+    return gulp.src(paths.app.html)
+        .pipe(minifyHTML(options))
+        .pipe(gulp.dest(paths.dist.html));
+});
+
+gulp.task('sass', function () {
+    gulp.src(paths.app.sass)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(paths.dist.sass))
+        .pipe(reload({ stream: true }));
+});
+
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: paths.dist.html
+    });
+
+    gulp.watch(paths.app.sass, ['sass']);
+    gulp.watch(paths.app.html, ['html']).on('change', reload);
+});
