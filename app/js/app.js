e886100c61f98665a36dbf51302e65e2a9f3ec8b
Matteo Rosati <mrosati@h-art.it>
2015-05-14 11:56:26 +0200

    add main app.js script file

---
+++ app/js/app.js
@@ -0,0 +1,7 @@
+(function() {
+    var titles = document.getElementsByTagName('h1'),
+        title = titles[0];
+
+    title.style.color = '#f00';
+    title.innerHTML = 'Colore and text updated via JS';
+}());
