54e72a6d7d5cc0efcc5838657aee86b2dbefca34
Matteo Rosati <mrosati@h-art.it>
2015-05-14 15:29:46 +0200

    add some examples

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,19 @@
 (function() {
-    var titles = document.getElementsByTagName('h1'),
-        title = titles[0];
+    var searchEventsForm = document.getElementById('search-events-form'),
+        events = document.getElementById('events'),
+        tplEventsSource = document.getElementById('tpl-events').innerHTML,
+        tplEvents = Handlebars.compile(tplEventsSource);
 
-    title.style.color = '#f00';
-    title.innerHTML = 'Colore and text updated via JS';
+    searchEventsForm.addEventListener('submit', function(ev) {
+        ev.preventDefault();
+
+        var searchEventsName = document.getElementById('search-events-name').value;
+
+        FB.api('/search/?q=' + searchEventsName + '&type=event', function(response) {
+            console.log(response);
+            events.innerHTML = tplEvents(response);
+        });
+
+        console.log(searchEventsName.value);
+    });
 }());
